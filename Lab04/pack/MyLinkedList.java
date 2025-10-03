351385a9a59c30d062c636ff844eef0f00908132
zoroza58-hue <zoroza58@gmail.com>
2025-10-03 12:26:30 +0000

    Update MyLinkedList.java

---
+++ Lab04/pack/MyLinkedList.java
@@ -0,0 +1,101 @@
+package pack
+
+public class MyLinkedList {
+    class Node {
+        private int data;
+        private Node next;
+
+        Node(int d) {
+            this.data = d;
+            next = null;
+        }
+    }
+    private Node head = null;
+    private int size = 0;
+
+    public int size() { return size; }
+
+    public void add(int d) {
+        Node p = new Node(d);
+        if (head == null) {
+            head = p;
+            size++;
+        }
+
+        p.next = head;
+        head = p;
+        size++;
+    }
+
+    public void insert(int d) {
+        Node newNode = new Node(d);
+
+        if (head == null || head.data >= d) {
+            newNode.next = head;
+            head = newNode;
+            size++;
+            return;
+        }
+
+        Node p = head;
+        while (p.next != null && p.next.data < d) {
+            p = p.next;
+        }
+
+        newNode.next = p.next;
+        p.next = newNode;
+        size++;
+    }
+
+    public int find(int d) {
+        Node p = head;
+        int i = 0;
+
+        while (p != null) {
+            if (p.data == d) {
+                return i;
+            }
+            p = p.next;
+            i++;
+        }
+        return -1;
+    }
+
+    public void delete(int d) {
+        if (head == null) { return; }
+
+        Node temp = new Node(0);
+        temp.next = haed;
+        Node p = temp;
+
+        While (p.next != null && p.next.data != d) {
+            p = p.next;
+        }
+        if (p.next != null) {
+            p.next = p.next.next;
+        }
+
+        head = temp.next;
+    }
+
+    public void setAt(int index) {
+
+    }
+
+    public void getAt(int index) {
+
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("head");
+        Node p = head;
+        while (p != null) {
+            sb.append("--> [");
+            sb.append(p.data);
+            sb.append("]");
+            p = p.next;
+        }
+        sb.append("--> null");
+        return new String(sb);
+    }
+}
